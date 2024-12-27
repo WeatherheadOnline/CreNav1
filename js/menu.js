0a7ead575cf9a8b6ce7ca671ece53dda8c07e9c1
WeatherheadOnline <WeatherheadOnline@gmail.com>
2024-12-26 21:46:53 -0700

    Document dims when menu is active

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("menu");
 const closeButton = document.getElementById("closeMenu");
 const hamburger = document.getElementById("hamburger");
+const main = document.getElementsByTagName("main")[0];
 let toggle = false;
 
 hamburger.addEventListener("click", toggleMenu);
@@ -10,9 +11,13 @@ function toggleMenu() {
     if (toggle) {
         menu.style.display = "none";
         toggle = false;
+        main.style.filter = "brightness(1)";
+        document.body.style.backgroundColor = "var(--aquaLt)";
     }
     else {
         menu.style.display = "block";
         toggle = true;
+        main.style.filter = "brightness(0.6)";
+        document.body.style.backgroundColor = "#6ecacf";
     }
 }
