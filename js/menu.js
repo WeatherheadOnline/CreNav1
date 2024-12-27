d88dbad03b1ee99e019d706c544af81798a1c6ef
WeatherheadOnline <WeatherheadOnline@gmail.com>
2024-12-27 10:27:21 -0700

    Animated menu

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ const menu = document.getElementById("menu");
 const closeButton = document.getElementById("closeMenu");
 const hamburger = document.getElementById("hamburger");
 const main = document.getElementsByTagName("main")[0];
+const footer = document.getElementsByTagName("footer")[0];
 let toggle = false;
 
 hamburger.addEventListener("click", toggleMenu);
@@ -9,15 +10,64 @@ closeButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     if (toggle) {
-        menu.style.display = "none";
+        turnOff();
+        dimmerOff();
         toggle = false;
-        main.style.filter = "brightness(1)";
-        document.body.style.backgroundColor = "var(--aquaLt)";
     }
     else {
-        menu.style.display = "block";
+        turnOn();
+        dimmerOn();
         toggle = true;
-        main.style.filter = "brightness(0.6)";
-        document.body.style.backgroundColor = "#6ecacf";
     }
 }
+
+function dimmerOff() {
+    document.body.style.animation = "brightenBkgdColor 1s 1";
+    main.style.animation = "brightenFilter 1s 1";
+    footer.style.animation = "brightenFilter 1s 1";
+    document.body.style.backgroundColor = "var(--aquaLt)";
+    main.style.filter = "brightness(1)";
+    footer.style.filter = "brightness(1)";
+}
+
+function dimmerOn() {
+    document.body.style.animation = "dimBkgdColor 1s 1";
+    main.style.animation = "dimFilter 1s 1";
+    footer.style.animation = "dimFilter 1s 1";
+    document.body.style.backgroundColor = "#6ecacf";
+    main.style.filter = "brightness(0.6)";
+    footer.style.filter = "brightness(0.6)";
+}
+
+let id = null;
+
+function turnOn() {
+    let pos = -270;
+    clearInterval(id);
+    id = setInterval(frameOn, 8);
+
+    function frameOn() {
+        if (pos == 0) {
+            clearInterval(id);
+        } else {
+            pos += 10;
+            menu.style.right = `${pos}px`;
+        }
+    }
+}
+
+function turnOff() {
+    let pos = 0;
+    id = setInterval(frameOff, 8);
+    
+    function frameOff() {
+        if (pos == -270) {
+            clearInterval(id);
+        } else {
+            pos -= 10;
+            menu.style.right = `${pos}px`;
+        }
+    }
+}
+
+
